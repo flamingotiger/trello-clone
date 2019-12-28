72c89db4d8aebb6aad5a1442440b0601d6daff0b
honghakbum <hhbhong@gmail.com>
2019-12-28 21:16:16 +0900

    Updates

---
+++ precache-manifest.ba2d9a0e21b6fb5c53e7f82f207b9948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4bc9f484c24c3f6d8a58b9a329cafb",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "6095dd9c4d2fb5e1bec0",
+    "url": "/trello-clone/static/css/main.8f738075.chunk.css"
+  },
+  {
+    "revision": "6ad431d26b7fbbb41c48",
+    "url": "/trello-clone/static/js/2.d313e932.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/trello-clone/static/js/2.d313e932.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6095dd9c4d2fb5e1bec0",
+    "url": "/trello-clone/static/js/main.ce650fdf.chunk.js"
+  },
+  {
+    "revision": "751794906de79d4751d4",
+    "url": "/trello-clone/static/js/runtime-main.e2fc9f5b.js"
+  }
+]);
